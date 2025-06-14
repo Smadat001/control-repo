4cc6a44cfdbccef9f9ca7fc43502614fbef430a9
Smadat001 <sdsh.mdth@gmail.com>
2025-06-14 16:11:42 +0530

    test client role _1

--- site-modules/profile/manifests/lab.pp
+++ site-modules/profile/manifests/lab.pp
@@ -1,3 +1,7 @@
 class profile::lab {
   # include coffeelabs_package
+  notify { 'LAB Profile':
+    # This is a simple notification to indicate that the lab profile is being applied.
+    message => 'This is the lab profile',
+  }
 }
