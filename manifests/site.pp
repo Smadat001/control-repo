77321b6b87e2950b2070141d999a2cacdff8abdb
Smadat001 <sdsh.mdth@gmail.com>
2025-07-10 13:39:00 +0530

    add mod chnages to dev1

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,7 +25,9 @@ File { backup => false }
 #
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+  if $facts['networking']['fqdn'] == 'puppet.coffeelabs.com' {
+    include profile::peserver
+  } else {
+    include profile::lab
+  }
 }
---
+++ site-modules/profile/manifests/lab.pp
@@ -0,0 +1,8 @@
+# It defines the lab profile for the coffeelabs project.
+class profile::lab {
+  include coffeelabs_package
+  notify { 'LAB Profile':
+    # This is a simple notification to indicate that the lab profile is being applied.
+    message => 'This is the lab profile',
+  }
+}
